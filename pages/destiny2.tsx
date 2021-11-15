69d4e52590dbedca9e8856e68c7300ee671e6271
darrenMabbayad <mabbayadd@gmail.com>
2021-11-15 03:12:17 -0500

    test with pokemon api

--- pages/destiny2.tsx
+++ pages/destiny2.tsx
@@ -1,26 +1,27 @@
 import { getDestinyDefinitionsObject, getManifest } from "utils/asyncHandlers";
 
 interface Props {
-  dataAsArr: Array<Object>;
+  pokemon: any;
 }
 
-const Destiny2 = ({ dataAsArr }: Props) => {
-  const [
-    inventoryItems,
-    equipSlots,
-    socketCategories,
-    itemCategories,
-    plugSets,
-    stats,
-    sandboxPerks,
-    talentGrids,
-    collectibles,
-  ] = dataAsArr;
+const Destiny2 = ({ pokemon }: Props) => {
+  // const [
+  //   inventoryItems,
+  //   equipSlots,
+  //   socketCategories,
+  //   itemCategories,
+  //   plugSets,
+  //   stats,
+  //   sandboxPerks,
+  //   talentGrids,
+  //   collectibles,
+  // ] = dataAsArr;
+  console.log(pokemon);
 
   return (
     <div>
       <p>Testing Stuff</p>
-      <button onClick={() => console.log(inventoryItems)}>Inventory</button>
+      {/* <button onClick={() => console.log(inventoryItems)}>Inventory</button>
       <button onClick={() => console.log(equipSlots)}>Slots</button>
       <button onClick={() => console.log(socketCategories)}>Sockets</button>
       <button onClick={() => console.log(itemCategories)}>ItemCats</button>
@@ -28,36 +29,39 @@ const Destiny2 = ({ dataAsArr }: Props) => {
       <button onClick={() => console.log(stats)}>Stats</button>
       <button onClick={() => console.log(sandboxPerks)}>Perks</button>
       <button onClick={() => console.log(talentGrids)}>Talents</button>
-      <button onClick={() => console.log(collectibles)}>Collectibles</button>
+      <button onClick={() => console.log(collectibles)}>Collectibles</button> */}
     </div>
   );
 };
 
 export async function getStaticProps() {
-  const manifest = await getManifest();
-  const data = manifest.Response.jsonWorldComponentContentPaths.en;
-  const definitions = [
-    data.DestinyInventoryItemDefinition,
-    data.DestinyEquipmentSlotDefinition,
-    data.DestinySocketCategoryDefinition,
-    data.DestinyItemCategoryDefinition,
-    data.DestinyPlugSetDefinition,
-    data.DestinyStatDefinition,
-    data.DestinySandboxPerkDefinition,
-    data.DestinyTalentGridDefinition,
-    data.DestinyCollectibleDefinition,
-  ];
+  // const manifest = await getManifest();
+  // const data = manifest.Response.jsonWorldComponentContentPaths.en;
+  // const definitions = [
+  //   data.DestinyInventoryItemDefinition,
+  //   data.DestinyEquipmentSlotDefinition,
+  //   data.DestinySocketCategoryDefinition,
+  //   data.DestinyItemCategoryDefinition,
+  //   data.DestinyPlugSetDefinition,
+  //   data.DestinyStatDefinition,
+  //   data.DestinySandboxPerkDefinition,
+  //   data.DestinyTalentGridDefinition,
+  //   data.DestinyCollectibleDefinition,
+  // ];
 
-  const dataAsArr = await Promise.all(
-    definitions.map(async (key: string) => {
-      const dataset = await getDestinyDefinitionsObject(key);
-      return dataset;
-    })
-  );
+  // const dataAsArr = await Promise.all(
+  //   definitions.map(async (key: string) => {
+  //     const dataset = await getDestinyDefinitionsObject(key);
+  //     return dataset;
+  //   })
+  // );
+
+  const res = await fetch("https://pokeapi.co/api/v2/pokemon/charmander");
+  const pokemon = await res.json();
 
   return {
     props: {
-      dataAsArr,
+      pokemon,
     },
   };
 }
