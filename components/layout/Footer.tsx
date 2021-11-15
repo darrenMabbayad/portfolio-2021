c61f5b5b7e3f5d4834711faf806e211b3ced6685
darrenMabbayad <mabbayadd@gmail.com>
2021-11-14 21:49:34 -0500

    tooltip for email

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -6,11 +6,21 @@ import {
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from "@styles/components/layout/Footer.module.scss";
 import urls from "@constants/urls";
+import { useState } from "react";
+import Tooltip from "@components/common/Tooltip";
 
 const Footer = () => {
+  const [showTooltip, setShowTooltip] = useState<boolean>(false);
+
   return (
     <footer className={styles.footer} id="contact">
-      <a className={styles.link} href="mailto:mabbayadd@gmail.com">
+      <a
+        className={`${styles.link} ${styles.linkEmail}`}
+        href="mailto:mabbayadd@gmail.com"
+        onMouseEnter={() => setShowTooltip(true)}
+        onMouseLeave={() => setShowTooltip(false)}
+      >
+        {showTooltip && <Tooltip>mabbayadd@gmail.com</Tooltip>}
         <FontAwesomeIcon className={styles.linkIcon} icon={faEnvelope} />
         <p className={styles.linkText}>Contact Me</p>
       </a>
